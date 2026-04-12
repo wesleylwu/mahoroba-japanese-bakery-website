ee7048dc78c735b6dac8df2df007f318e877a9dc
xxiyun <wesleylwu@gmail.com>
2026-04-12 15:39:17 -0700

    fix layout.tsx errors

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -4,33 +4,34 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
 export async function POST(
   req: NextRequest,
   { params }: { params: Promise<{ orderId: string }> },
 ) {
   const { orderId } = await params;
 
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
+
+  if (!process.env.STRIPE_SECRET_KEY) {
+    return new NextResponse(
+      JSON.stringify({ message: "Stripe secret key is missing" }),
+      { status: 500 },
+    );
+  }
+
   const order = await prisma.order.findUnique({
-    where: {
-      id: orderId,
-    },
+    where: { id: orderId },
   });
 
   if (order) {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: Number(order.price) * 100,
       currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
+      automatic_payment_methods: { enabled: true },
     });
 
     await prisma.order.update({
-      where: {
-        id: orderId,
-      },
+      where: { id: orderId },
       data: { intent_id: paymentIntent.id },
     });
 
